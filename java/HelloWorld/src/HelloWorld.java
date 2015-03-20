a0139f690aa3fe672381bd257aba35044836af9b
HSRT-CHMA <Christoph.Mayer@Student.reutlingen-university.de>
2015-03-20 12:20:51 +0100

    Java Hello Wolrd
    First Java Hello World Programm with Eclipse Juno Workspace

---
+++ java/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * First Program Hello World
+ * @author Christoph Mayer
+ *
+ */
+
+public class HelloWorld {
+	
+	/**
+	 * Main function for Hello Wolrd
+	 * @param args String Array for arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
