dee1cfbc917001527d30887ce3fb804aeda66bd4
watto1112 <martin.watolla@hotmail.de>
2015-03-27 12:41:26 +0100

    C++ TestProjekt
    Übungsaufgabe in c++

---
+++ cpp/TestProject/vorlesungsUebung/vorlesungsUebung.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+	
+	string notiz;
+	string aufgaben;
+
+	cout << "Bitte tragen Sie eine Notiz ein" << endl;
+	cin >> notiz;
+
+	cout << "Bitte tragen Sie ein Aufgabe ein" << endl;
+	cin >> aufgaben;
+
+	cout << notiz << endl;
+	cout << aufgaben << endl;
+
+	
+
+	return 0;
+}
+
