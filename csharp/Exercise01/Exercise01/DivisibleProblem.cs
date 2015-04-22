1eb41298acaa58729addd4e4dc90ba2c1557e67b
HSRT-CHMA <Christoph.Mayer@Student.reutlingen-university.de>
2015-04-22 15:59:52 +0200

    C# Commit for further changes from M and G

---
+++ csharp/Exercise01/Exercise01/DivisibleProblem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    class DivisibleProblem {
+        public Boolean isDirectlySolvable(){
+            return false;
+        }
+    }
+}
--- csharp/Exercise01/Exercise01/GPS.cs
+++ csharp/Exercise01/Exercise01/GPS.cs
@@ -9,5 +9,24 @@ namespace Exercise01{
      * Class for General Problem Solver
      */
     class GPS{
+
+        /*
+         * Default Constructor of General Problem Solver
+         */
+        public GPS(){
+           
+        }
+
+        public Solution getSolution(){
+            return null;
+        }
+
+        public void part(){
+
+        }
+
+        public void solve(){
+
+        }
     }
 }
