a1f2432dab02dbec3f6428398eb3ec9337bba6f2
HSRT-CHMA <Christoph.Mayer@Student.reutlingen-university.de>
2015-05-02 15:54:20 +0200

    C# Update Delete Method
    Update from Delete Method but with little Bug. I'm working on it but i
    wouldn't withhold to you ;-)

--- csharp/Exercise01/Exercise01/Program.cs
+++ csharp/Exercise01/Exercise01/Program.cs
@@ -17,19 +17,50 @@ namespace Exercise01
         {
             static void Main(string[] args)
             {
-                //Creating Binary Object with Values: 
-                Console.WriteLine("Welcome ");
+                /*
+                 * Create BinaryTree with Root 4
+                 * and the Values: 3,5,-2,33
+                 */
                 BinaryTree tree = new BinaryTree(4);
                 tree.Insert(3);
                 tree.Insert(5);
                 tree.Insert(-2);
                 tree.Insert(33);
+
+                /*
+                 * Test of Has Method for the Values
+                 * 3 -> true
+                 * 5 -> true
+                 * 199 -> false
+                 */
                 Console.WriteLine("   " + tree.HasValue(3));
                 Console.WriteLine("   " + tree.HasValue(5));
                 Console.WriteLine("   " + tree.HasValue(199));
+                
+                /*
+                 * Test of Delete Method for the Value 5
+                 * Has Method should now be false
+                 */
+                Console.WriteLine("   " + tree.Delete(5));
+                Console.WriteLine("   " + tree.HasValue(5));
+
+                /*
+                 * Test of GetSmallest and GetBiggest Method
+                 * Smallest Value is -2
+                 * Biggest Value is 33
+                 */
+                Console.WriteLine("   " + tree.GetBiggest().GetValue());
+                Console.WriteLine("   " + tree.GetSmallest().GetValue());
+                 
+                
+                /*
+                 * That the console output is visible
+                 */
                 Console.ReadLine();
 
-                //Creating GPS Object with Values: 
+                /*
+                 * Creating test for the General Problem Solver
+                 */
                 GPS pSolver = new GPS(tree);
             }
         }
