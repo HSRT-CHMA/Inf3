85550f69f08710c88f27080078d30b15a094095b
GeorgiaKing <georgia.m.koenig@gmail.com>
2015-05-20 02:05:07 +0200

    Python -Second Try on Parser
    Deleted some classes ; Added Class P_Parser --> better version of
    Parser, but not perfect yet, still the brackets-bug

--- python/Exersice02/Exersice02/Exersice02.py
+++ python/Exersice02/Exersice02/Exersice02.py
@@ -1,21 +1,25 @@
 #print('Hello World')
 import sys
 import Parser
+import P_Parser
 
 if __name__ == '__main__':
     #print("Main-Method")
     # command line arguments can be altered via Debuggen > Exersice02 Eigenschaften > Debug > Script Arguments
-    if len(sys.argv) != 1 :
-        for i in range(len(sys.argv)):
-            if i == 0:
-                print("Programm-name: %s" % sys.argv[0])
-            else:
-                print("%d. Argument: %s" % (i,sys.argv[i]))
-    else :
-        print("ERROR : No command line arguments have been selected")
+    #if len(sys.argv) != 1 :
+        #for i in range(len(sys.argv)):
+            #if i == 0:
+                #print("Programm-name: %s" % sys.argv[0])
+            #else:
+                #print("%d. Argument: %s" % (i,sys.argv[i]))
+    #else :
+        #print("ERROR : No command line arguments have been selected")
 
     print("______________________________________________________")
-    my_parser = Parser.Parser()
+    #my_parser = Parser.Parser()
+    
+    my_other_parser = P_Parser.P_Parser()
     for i in range(1, len(sys.argv)):
-        #print(my_parser.parse(sys.argv[i]))
-        print(my_parser.parse(sys.argv[i]))
+        #print("Argument at position " + str(i) + " is " + my_other_parser.parseConstant(sys.argv[i]) )
+        #print("%d. Argument: %s" % (i,sys.argv[i]) + " | The Result of parse() is " + str(my_other_parser.parse(sys.argv[i])))
+        print(my_other_parser.parse(sys.argv[i]))
--- python/Exersice02/Exersice02/P_Parser.py
+++ python/Exersice02/Exersice02/P_Parser.py
@@ -1,15 +1,6 @@
-
-class Parser(object):
+class P_Parser(object):
     """description of class"""
 
-    def __init__(self):
-        #params might be added
-        '''
-        Constructor
-        '''
-
-    # Add some more general methods to check the incomming string 
-    #e.g. BracketProblem; ExpressionStarts/EndsWithAnOperator; MoreThan1=Problem; and so on
     def check_input(self, line_input) :
         # None is equivalent to Void or Null
         assert line_input != None
@@ -22,7 +13,7 @@ class Parser(object):
 
         return bol
     
-
+    '''' This Method organises the final output of the calss P_Parser'''
     def parse(self, line_input):
         # None is equivalent to Void or Null
         assert line_input != None
@@ -31,7 +22,6 @@ class Parser(object):
         print(line_input)
         final_output = ""
         res = ""
-
 
         if self.check_input(line_input) :
             final_output = self.parseEquation(line_input) # Check the given input
@@ -49,24 +39,19 @@ class Parser(object):
             res = "ERROR"
 
         return res
-   
+
+    
+    
     ''' Method checks if the given string is a valid Equation (An Expression ; optional followed by "=" , and one more Expression )
-        Method returns None if Equation is in any form invalid, or True/False or int-Value of Expression '''
-    def parseEquation(self, line_input):
-         # None is equivalent to Void or Null
+    Method returns None if Equation is in any form invalid, or True/False or int-Value of Expression'''
+    def parseEquation(self, line_input) :
+        # None is equivalent to Void or Null
         assert line_input != None
         # Precondition line_input is a String
         assert type(line_input) == str 
-        print(line_input)
-        result_eq = ""
-        # A String to show the outcome of this Method ( can be None if invalid, 
-        # True/False as a value for Equality of the Equation or a int-Value of the Expression )
-        left = 0
-        right = 0
-        # Int-Variables to be used as results
-
+        
         if '=' in line_input : # Is input an Equation or an Expression ?
-            print("Its an Equation !")
+            #print("Its an Equation !")
             equation_elements = line_input.split('+') 
             #Split the string at the '=' - Operator
             left = equation_elements[0]
@@ -74,10 +59,10 @@ class Parser(object):
             right = equation_elements[1]
             # The second, right part of the Equation
             result = ""
+
             # To be used to show the outcome of the parseExpression-Method
             result_left = self.parseExpression(left)
             result_right = self.parseExpression(right)
-
 
             if result_left != None and result_right != None : # Both parts of the Equation are valid
                 if int(result_left) == int(result_right) : # Both parts of the equation are of the same value
@@ -94,87 +79,69 @@ class Parser(object):
                 # Why here ERROR ?
                 result_int = 0
                 result_int = result_int + int(result)
-                print("Line 84  int_result : " + str(result_int))
                 result_eq = str(result_int)
-                print("Its a int-Value !")
             else :
                 result_eq = None
 
         return result_eq
     
     
-    ''' Method checks if the given string is a valid Expression (A single Term | Term + Term + ....+ Term )'''    
-    def parseExpression(self, line_input):
-         # None is equivalent to Void or Null
+    def parseExpression(self,line_input) :
+        # None is equivalent to Void or Null
         assert line_input != None
         # Precondition line_input is a String
         assert type(line_input) == str 
-        #result = None or result = valueOfTheExpression
+        result_int = 0
+        # The int value of a Expression 
         result_string = ""
-        #String to show the outcome of this Method
-        bol = True 
-        ## boolean-variable to interpret the result; must be set true at the beginning
-        numeric_result = 0
-        # Int-value to show the result of the addition of all Terms in the given Expression
-        # It is 0 because that is the neutral element in Addition
-        no_brackets = ""
-        # The version of line_input with no brackets in it
-        
-        print("This is the input for parseExpression : " + line_input)
+        # Used to show the outcome of this Method ; can be None if invalid Expression; or combined value of the Expression
+        #print(line_input)
+        bol = True
 
-        # Starts and Ends the line_input with brackets ?
-        if line_input.endswith(")") and line_input.startswith("("):
-            print("Its a 'Factor-Expression' !")
-            no_brackets = line_input[1:len(line_input)-1]
-            print("No brackets input : " + no_brackets)
-            # Does it contains no + or multiple + ?
-            if '+' in no_brackets :
-                print("Multiple Terms")
-                all_term = no_brackets.split('+') 
-                for i in range(0, len(all_term)) :
-                    print("Line 122")
-                    if self.parseTerm(all_term[i]) != None :
-                        #Note : if the seperated Term is valid; True and Treu = True 
-                        bol = bol and True
-                        #int(a) returns the corresponding int-value of string a
-                        numeric_result = numeric_result + int(self.parseTerm(all_term[i])) 
-                        #--> Caution : Does not work if still brackets in string
-                    else :
-                        # if the seperated Term is not valid ; True and False = False and stays False !
-                        bol = bol and False
+        if '+' in line_input :
+            #print("Multiple Terms")
+            all_term = line_input.split('+')
+            for i in range(0, len(all_term)):
+                if self.parseTerm(all_term[i]) != None : # Are all Terms valid ? 
+                    bol = bol and True 
+                else :
+                    bol = bol and False
 
-
+        else :
+            #print("Single Term") 
+            result_tmp = self.parseTerm(line_input) # String or None
+            if result_tmp != None :
+                bol = True
             else :
-                print("Single Term")
-                if self.parseTerm(line_input) != None :
-                    numeric_result = self.parseTerm(line_input)
-                    # If Term is invalid ; result_string becomes None; 
-                    # else it becomes the value of the single Factor; or combined value of all Factors
-                else : 
-                    result_string = None
-        else : 
-            print("Its a normal Equation-Expression")
+                bol = False
 
-        if bol : # Valid Term(s)
-            result_string = numeric_result
-            print("The numeric value of line_input " + line_input + " is " + str(numeric_result))
+        if bol : # Valid Expression - Now find the result 
+            if '+' in line_input :
+                # result_string is all the Term-Results in addition
+                all_term = line_input.split('+')
+                for i in range(0, len(all_term)):
+                    result_int = result_int + int(self.parseTerm(all_term[i]))
+                #Not in the loop
+                result_string = str(result_int)
+            else : 
+                # result_string is the single value of this single Term 
+                result_string = self.parseTerm(line_input)
         else :
             result_string = None
 
-        return result_string
+        return result_string # Can be None or value of Expression
     
-        
-        
     ''' Method checks if the given string is a valid Term (A single factor | a factor * factor ' ... * factor)'''
     def parseTerm(self, line_input):
-         # None is equivalent to Void or Null
+        # None is equivalent to Void or Null
         assert line_input != None
         # Precondition line_input is a String
         assert type(line_input) == str 
         #result_t = None
         #Term-Object to show the outcome of this Method
         result_string = ""
-        #String to show the outcome of this Method
+        #String to show the outcome of this Method; can be None if Factor(s) are invalid or the value of a single Factor, 
+        # or the combined values of all Factors in a Term
         bol = True 
         ## boolean-variable to interpret the result; must be set true at the beginning
         numeric_result = 1
@@ -182,6 +149,7 @@ class Parser(object):
         # It is 1 because that is the neutral element in Multiplication
         
         if '*' in line_input : # Term contains multiple Factors
+            #print("Multiple Factors")
             all_term = line_input.split('*') 
             #Split the string at the * - Operator
             for i in range(0, len(all_term)) :
@@ -196,18 +164,18 @@ class Parser(object):
         
             if bol : # Valid Factor
                 result_string = numeric_result
-                print("The numeric value of line_input " + line_input + " is " + str(numeric_result))
+                #print("The numeric value of line_input " + line_input + " is " + str(numeric_result))
             else :
                 result_string = None
 
         else : # Term contains a single Factor, which has to be checked 
+            #print("Single factor")
             result_string = self.parseFactor(line_input)
             # If Factor is invalid ; result_string becomes None; 
             # else it becomes the value of the single Factor
-        return result_string
+        return result_string 
         
-        
-    
+
     ''' Method checks if input is a valid Factor (A Constant or an Expression)
     parms line_input: The given string to be checked '''
     def parseFactor(self, line_input):
@@ -232,8 +200,8 @@ class Parser(object):
         return result_string 
         # Result_string can be None or a string containing the value of the Constant; 
         # or a string containing the value of the Expression in the Factor
-            
-        
+
+
     ''' Method checks if input is a valid Constant (e.g. 1, 50, 908 etc)  
     or if it is not valid like : 0, 099, etc
     parms line_input: The given string to be checked'''
@@ -247,7 +215,7 @@ class Parser(object):
         #result_c = None
         # To be used as the result-Object to show the outcome of this parse-Method
         result_string = ""
-        # String to show the outcome of 
+        # String to show the outcome of this Method
         first_valid = self.isDigitWOZero(line_input[0])
         # The first number is checked seperatly; must not be a '0' 
         
@@ -260,17 +228,12 @@ class Parser(object):
             else :
                 bol = False
                 
-        if bol : # If line_input is a valid Constant
-            # From valid string, a Constant-Object is created
-            #valid_constant = Constant.Constant(line_input)
-            #result_c = valid_constant
-            result_string = line_input
+        if bol : 
+            result_string = line_input # result is "value" of the constant if valid one
         else :
-            result_string = None
+            result_string = None # result is None if constant is not valid 
 
         return result_string
-        
-        
     
     '''Method checks if input is a valid digit (0 - 9)
     parms line_input: The string to be checked'''
@@ -282,7 +245,9 @@ class Parser(object):
         return line_input.isdigit()
         #str : isdigit() Return true if all characters in the string are digits (0-9)
         #and there is at least one character, false otherwise. 
-        
+    
+    
+    
     ''' This Method checks if input is a valid DigitWOZero (1-9) 
     parms line_input: The string to be checked'''    
     def isDigitWOZero(self, line_input):
@@ -303,8 +268,3 @@ class Parser(object):
         
         return bol   
         #assert type(bol) == bool
-
-
-
-
-
